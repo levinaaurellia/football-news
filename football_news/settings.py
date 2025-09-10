58eb16c0de669f869ec74cf7d62e54159ed7aea3
levinaaurellia <emailkamu@example.com>
2025-09-10 14:37:53 +0700

    tutorial 2 selesai

--- football_news/settings.py
+++ football_news/settings.py
@@ -33,6 +33,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "levina-aurellia-footballnews.pbp.cs.ui.ac.id"]
 
+CSRF_TRUSTED_ORIGINS = [
+    "https://levina-aurellia-footballnews.pbp.cs.ui.ac.id"
+]
 
 # Application definition
 
@@ -61,7 +64,7 @@ ROOT_URLCONF = 'football_news.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'], # Tambahkan konten baris ini
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
